88dde7bbd62832a07cc52db691ea06774a713535
Miguelito2926 <juniorprol@hotmail.com>
2023-07-08 18:09:57 -0300

    Criando entidade Product

--- demo/src/main/java/com/edsuperior/demo/entities/Department.java
+++ demo/src/main/java/com/edsuperior/demo/entities/Department.java
@@ -3,3 +3,29 @@ package com.edsuperior.demo.entities;
 public class Department {
 
 	private Long id;
+	private String name;
+	
+	
+	public Department() {
+	}
+	//Encapsulamento
+	public Department(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
